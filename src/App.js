d473265710ad48d93217948b937fc3fdd6a9b1f7
Stefano Miceli <mic@nucleode.com>
2020-07-29 09:52:21 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,75 +5,24 @@ import CartIndicator from "./components/CartIndicator";
 import BookStore from "./components/BookStore";
 import Cart from "./components/Cart";
 
-class App extends Component {
-  state = {
-    cart: {
-      products: [],
-    },
-  };
-
-  handleAddToCart = (id) => {
-    this.setState({
-      ...this.state,
-      cart: {
-        ...this.state.cart,
-        products: [...this.state.cart.products, id],
-      },
-    });
-  };
-
-  handleRemoveFromCart = (id) => {
-    const bookToRemove = this.state.cart.products.findIndex(
-      (bookId) => bookId === id
-    );
-
-    this.setState({
-      ...this.state,
-      cart: {
-        ...this.state.cart,
-        products: [
-          ...this.state.cart.products.slice(0, bookToRemove),
-          ...this.state.cart.products.slice(bookToRemove + 1),
-        ],
-      },
-    });
-  };
-
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12 text-center background-div">
-            <Link to="/">
-              <h1>Strivazon Book Store</h1>
-            </Link>
-          </div>
-          <CartIndicator cartItemsNum={this.state.cart.products.length} />
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-12 text-center background-div">
+          <Link to="/">
+            <h1>Strivazon Book Store</h1>
+          </Link>
         </div>
-        <hr />
-        <div className="container">
-          <Route
-            path="/"
-            exact
-            render={(props) => (
-              <BookStore {...props} addToCart={this.handleAddToCart} />
-            )}
-          />
-          <Route
-            path="/cart"
-            exact
-            render={(props) => (
-              <Cart
-                {...props}
-                cart={this.state.cart.products}
-                removeFromCart={this.handleRemoveFromCart}
-              />
-            )}
-          />
-        </div>
+        <CartIndicator />
       </div>
-    );
-  }
+      <hr />
+      <div className="container">
+        <Route path="/" exact component={BookStore} />
+        <Route path="/cart" exact component={Cart} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
